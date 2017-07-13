77d3124e89649ee84d769701b2f00aa2ee6d1a1c
SonTrungTo <son.trung.to@gmail.com>
2017-07-14 01:19:04 +0300

    Prog5_8.c in C_Kochan done!

--- beginner/C_Kochan/prog5_8.c
+++ beginner/C_Kochan/prog5_8.c
@@ -1,1 +1,19 @@
 // Program to reverse the digits of a number
+#include <stdio.h>
+
+int main(void) {
+  int number, digit;
+
+  printf("Enter an integer:\n");
+  scanf("%i", &number);
+
+  while (number != 0) {
+    digit = number % 10;
+    printf("%i", digit);
+    number /= 10;
+  }
+
+  printf("\n");
+
+  return 0;
+}
