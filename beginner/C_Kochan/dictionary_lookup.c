db521ccecf210911347cb03191927fd59fb73c10
SonTrungTo <son.trung.to@gmail.com>
2017-09-06 02:24:56 +0300

    Add simple dictionary lookup program

--- beginner/C_Kochan/dictionary_lookup.c
+++ beginner/C_Kochan/dictionary_lookup.c
@@ -14,7 +14,7 @@ struct entry {
 bool equalStrings(const char s1[], const char s2[]) {
   int i = 0;
 
-  while (s1[i] = s2[i] && s1[i] != '\0' && s2[i] != '\0')
+  while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
     ++i;
 
   if (s1[i] == s2[i])
@@ -28,11 +28,43 @@ bool equalStrings(const char s1[], const char s2[]) {
 int lookup(const struct entry dictionary[], const char search[],
            const int entries) {
   int  i;
-  bool equalStrings(const char s1[], const char s2[]);  
+  bool equalStrings(const char s1[], const char s2[]);
 
   for (i = 0; i < entries; i++)
-    if (equalStrings(search, dictionary[i].definition))
+    if (equalStrings(search, dictionary[i].word))
       return i;
 
   return -1;
+}
+
+int main(void) {
+  const struct entry dictionary[100] = {
+    {"aardvark", "a burrowing African mammal"       },
+    {"abyss",    "a bottomless pit"                 },
+    {"acumen",   "mentally sharp; keen"             },
+    {"addle",    "to become confused"               },
+    {"aerie",    "a high nest"                      },
+    {"affix",    "to append; attach"                },
+    {"agar",     "a jelly made from seaweed"        },
+    {"ahoy",     "a nautical call of greeting"      },
+    {"aigrette", "an ornamental cluster of feathers"},
+    {"ajar",     "partially opened"}
+  };
+
+  int  entry;
+  int  entries = 10;
+  char word[10];
+  int  lookup(const struct entry dictionary[], const char search[],
+              const int entries);
+
+  printf("Enter your search: ");
+  scanf("%9s", word);
+  entry = lookup(dictionary, word, entries);
+
+  if (entry != -1)
+    printf("%s\n", dictionary[entry].definition);
+  else
+    printf("The word \"%s\" is not found in the database\n", word);
+
+  return 0;
 }
