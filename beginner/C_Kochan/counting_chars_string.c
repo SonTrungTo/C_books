e51292edfa1a3c5d9f4e2daba8a15305eb0ab490
SonTrungTo <son.trung.to@gmail.com>
2017-09-05 23:13:01 +0300

    Add more info about stringLength()
    - escape characters and the value of special char, the null char,
    and that of other chars.

--- beginner/C_Kochan/counting_chars_string.c
+++ beginner/C_Kochan/counting_chars_string.c
@@ -7,7 +7,7 @@
 int stringLength(const char string[]) {
   int count = 0;
 
-  while (string[count] != '\0')
+  while (string[count]) // Use the fact that '\0' == 0 and other chars are not 0
     ++count;
 
   return count;
@@ -18,9 +18,10 @@ int main(void) {
   const char  word1[] = {'K','o','y','o','m','i','\0'};
   const char  word2[] = {'1','+','1','\0'};
   const char  word3[] = {'F','I','\0'};
+  const char  word4[] = "\033\"Hello\"\n";
 
-  printf("%i  %i  %i", stringLength(word1), stringLength(word2),
-stringLength(word3));
+  printf("%i  %i  %i  %i", stringLength(word1), stringLength(word2),
+stringLength(word3), stringLength(word4));
 
   return 0;
 }
