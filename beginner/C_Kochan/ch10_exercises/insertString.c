ee69908abfb6bf0cffd13a4254c15bafabe67a4c
SonTrungTo <son.trung.to@gmail.com>
2017-09-10 00:21:49 +0300

    insertString() temporarily solved!
    - stash smashing detected!

---
+++ beginner/C_Kochan/ch10_exercises/insertString.c
@@ -0,0 +1,63 @@
+/**
+*   insertString(text,"string",position)
+* Function to insert a string inside the text string at the designated position.
+*/
+#include <stdio.h>
+
+// Let's reuse the concat from concatenating_char_arrays_revised.c
+void concat(char result[], char str1[], char str2[]) {
+  int i, j;
+
+  for (i = 0; str1[i]; i++)
+    result[i] = str1[i];
+
+  for (j = 0; str2[j]; j++)
+    result[j + i] = str2[j];
+
+  result[j + i] = '\0';
+}
+
+// Also reuse the substring function from substring.c
+void substring(char source[], int index, int count,
+               char result[]) {
+  int i, j = 0, numberOfElementsLeft;
+
+  while ( source[j] )
+    ++j;
+
+  j -= 1;
+  numberOfElementsLeft = j - index + 1;
+
+  if (count <= numberOfElementsLeft && count)
+    for (i = 0; i < count; i++)
+      result[i] = source[index + i];
+  else
+    for (i = 0; i < numberOfElementsLeft; i++)
+      result[i] = source[index + i];
+
+  result[i] = '\0';
+}
+
+// Let's write insertString
+void insertString(char source[], char str[], int position) {
+  void concat(char result[], char str1[], char str2[]);
+  void substring(char source[], int index, int count, char result[]);
+  int  i;
+  char result[50], result2[50];
+
+  substring(source,0,position,result);
+  substring(source,position,81,result2);
+  concat(result,result,str);
+  concat(source,result,result2);
+}
+
+int main(void) {
+  void insertString(char source[], char str[], int position);
+  char text[] = "the wrong son";
+
+  insertString(text,"per",10);
+
+  printf("%s\n", text);
+
+  return 0;
+}
