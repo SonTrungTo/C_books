f3e84970bdd2d1019a99d49b19a308a31c8f491c
SonTrungTo <son.trung.to@gmail.com>
2017-09-11 23:50:09 +0300

    strToFloat written!
    - It converts a string of float into a float number.
    - ex12, ch10 finished!

--- beginner/C_Kochan/ch10_exercises/strToFloat.c
+++ beginner/C_Kochan/ch10_exercises/strToFloat.c
@@ -26,32 +26,65 @@ int stringLength(const char source[]) {
   return count;
 }
 
-float strToFloat(const char string[]) {
+double strToFloat(const char string[]) {
   bool   isDot(const char string[]);
-  float  result = 0, resultF = 0;
+  double  result = 0, resultF = 0;
   int    i, intValue, resultI = 0, stringLength(const char source[]);
 
-  if (isDot) {
-    for (i = 0; string[i] != '.'; i++) {
-      intValue = string[i] - '0';
-      resultI  = resultI * 10 + intValue;
-    }
+  if (isDot(string)) {
+    if (string[0] == '-') {
+      for (i = 1; string[i] != '.' && string[i] >= '0' && string[i] <= '9'; i++) {
+        intValue = '0' - string[i];
+        resultI  = resultI * 10 + intValue;
+      }
 
-    for (i = stringLength(string) - 1; string[i] != '.'; i--) {
-      intValue = string[i] - '0';
-      resultF  = (resultF + intValue) / 10;
-    }
+      for (i = stringLength(string) - 1; string[i] != '.' && string[i] >= '0'
+      && string[i] <= '9'; i--) {
+        intValue = '0' - string[i];
+        resultF  = (intValue + resultF) / 10;
+      }
 
-    result = resultI + resultF;
+      result = resultI + resultF;
+    } else {
+      for (i = 0; string[i] != '.' && string[i] >= '0' && string[i] <= '9'; i++) {
+        intValue = string[i] - '0';
+        resultI  = resultI * 10 + intValue;
+      }
+
+      for (i = stringLength(string) - 1; string[i] != '.' && string[i] >= '0'
+      && string[i] <= '9'; i--) {
+        intValue = string[i] - '0';
+        resultF  = (resultF + intValue) / 10;
+      }
+
+      result = resultI + resultF;
+    }
+  }
+  else { // If there are no dots, the function acts like strToInt
+    if (string[0] == '-')
+      for (i = 1; string[i] >= '0' && string[i] <= '9'; i++) {
+        intValue = '0' - string[i];
+        result   = result * 10 + intValue;
+      }
+    else
+      for (i = 0; string[i] >= '0' && string[i] <= '9'; i++) {
+        intValue = string[i] - '0';
+        result   = result * 10 + intValue;
+      }
   }
 
   return result;
 }
 
 int main(void) {
-  float strToFloat(const char string[]);
+  double strToFloat(const char string[]);
 
   printf("%f\n", strToFloat("1.23"));
+  printf("%f\n", strToFloat("-867.6921"));
+  printf("%f\n", strToFloat("12345"));
+  printf("%f\n", strToFloat("-987654321"));
+  printf("%f\n", strToFloat("123xx456"));
+  printf("%f\n", strToFloat("xxx"));
 
   return 0;
 }
