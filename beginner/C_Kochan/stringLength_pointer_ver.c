36619008ce596e417c847a8caa05f713944b9660
SonTrungTo <son.trung.to@gmail.com>
2017-09-16 23:37:42 +0300

    Example 11.15: stringLength using pointer.
    - Using subtraction between pointers. (ptrdiff_t in <stddef.h>)

---
+++ beginner/C_Kochan/stringLength_pointer_ver.c
@@ -0,0 +1,21 @@
+// Example 11.15: stringLength function using pointers.
+#include <stdio.h>
+
+int stringLength(const char *string) {
+  const char *cptr = string;
+
+  while (*cptr)
+    ++cptr;
+
+  return cptr - string;
+}
+
+int main(void) {
+  int stringLength(const char *string);
+
+  printf("%i  ", stringLength("stringLength test"));
+  printf("%i  ", stringLength(""));
+  printf("%i \n", stringLength("complete"));
+
+  return 0;
+}
