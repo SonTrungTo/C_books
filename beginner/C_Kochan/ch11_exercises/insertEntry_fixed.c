a7787ed576ecafbbdfa6b501f3c106cd1fb78538
SonTrungTo <son.trung.to@gmail.com>
2017-09-18 00:52:30 +0300

    insertEntry() adds a list entry to after the current
    element. I have learnt a lot from this exercise: function
    cannot change pointer!

--- beginner/C_Kochan/ch11_exercises/insertEntry_fixed.c
+++ beginner/C_Kochan/ch11_exercises/insertEntry_fixed.c
@@ -2,10 +2,6 @@
 *  insertEntry(*entry_insert, *element_after);
 * This inserts a new entry in the linked list after the element specified
 * in the second argument.
-*
-* NOTE: This program does not work. This is my first attempt at solving the problem
-* and leaves me with a big headache on why the program does not work. Explanations
-* are below.
 */
 #include <stdio.h>
 
@@ -14,15 +10,14 @@ struct entry {
   struct entry *next;
 };
 
-void insertEntry(struct entry *listEntry, struct entry *next) {
-  listEntry->next = next;
-  next            = listEntry; // THIS: function cannot change the pointer 'next'
-// It can only change the data element it refers to (signified by the indirection operator *).
+void insertEntry(struct entry *listEntry, struct entry *current_element) {
+  listEntry->next         = current_element->next;
+  current_element->next   = listEntry;
 }
 
 int main(void) {
   struct entry n1, n2, n3, n2_3, *listPtr = &n1, *listEntry = &n2_3;
-  void         insertEntry(struct entry *listEntry, struct entry *next);
+  void         insertEntry(struct entry *listEntry, struct entry *current_element);
 
   n1.value = 100;
   n1.next  = &n2;
@@ -34,8 +29,17 @@ int main(void) {
   n3.next  = (struct entry *) 0;
 
   n2_3.value = 250;
-  insertEntry(listEntry, n2.next);
 
+  printf("The list before insertEntry is: \n");
+  while (listPtr != (struct entry *) 0) {
+    printf("%i\n", listPtr->value);
+    listPtr = listPtr->next;
+  }
+
+  listPtr = &n1;
+  insertEntry(listEntry, &n2);
+
+  printf("\nThe list after insertEntry is: \n");
   while (listPtr != (struct entry *) 0) {
     printf("%i\n", listPtr->value);
     listPtr = listPtr->next;
