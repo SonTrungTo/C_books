4e7ded2d8eb7d87528f76baa6c943f7e29981636
SonTrungTo <son.trung.to@gmail.com>
2017-09-22 00:44:01 +0300

    Exercise 6 solving.
    - addEntry written, but removeEntry is not.
    - Too sleepy to finish.

--- beginner/C_Kochan/ch11_exercises/doubleLinkedList.c
+++ beginner/C_Kochan/ch11_exercises/doubleLinkedList.c
@@ -1,5 +1,7 @@
 // Exercise 5: Constructing a new structure: the double linked list
+// Exercise 6: Constructing two functions: insertEntry and removeEntry.
 #include <stdio.h>
+#include <stdbool.h>
 
 struct entry {
   int          value;
@@ -7,8 +9,25 @@ struct entry {
   struct entry *previous;
 };
 
+// We make insertEntry add pointer to the new entry after the pointer to the old one
+// if next is true and before the pointer to the old one if otherwise.
+void insertEntry(struct entry *newEntry, struct entry *oldEntry, bool next) {
+  if (next) {
+    newEntry->next = oldEntry->next;
+    oldEntry->next = newEntry;
+  } else {
+    newEntry-> previous = oldEntry->previous;
+    oldEntry-> previous = newEntry;
+  }
+}
+
+void removeEntry(struct entry *listEntry) {
+  
+}
+
 int main(void) {
-  struct entry n1, n2, n3, *listNextPtr = &n1, *listPreviousPtr = &n3;
+  struct entry n1, n2, n3, n2_3, *listNextPtr = &n1, *listPreviousPtr = &n3;
+  void insertEntry(struct entry *newEntry,struct entry *oldEntry,bool next);
 
   n1.value    = 100;
   n1.next     = &n2;
@@ -22,6 +41,8 @@ int main(void) {
   n3.next     = (struct entry *) 0;
   n3.previous = &n2;
 
+  n2_3.value = 23579;
+
   printf("Next-order double linked list:\n");
   while (listNextPtr != 0) {
     printf("%i\n", listNextPtr->value);
@@ -29,6 +50,23 @@ int main(void) {
   }
 
   printf("Previous-order double linked list:\n");
+  while (listPreviousPtr != 0) {
+    printf("%i\n", listPreviousPtr->value);
+    listPreviousPtr = listPreviousPtr->previous;
+  }
+
+  listNextPtr = &n1;
+  listPreviousPtr = &n3;
+  insertEntry(&n2_3,&n2,true);
+  insertEntry(&n2_3,&n2,false);
+
+  printf("Inserting n2_3 after element n2 gives:\n");
+  while (listNextPtr != 0) {
+    printf("%i\n", listNextPtr->value);
+    listNextPtr = listNextPtr->next;
+  }
+
+  printf("Inserting n2_3 before element n2 gives:\n");
   while (listPreviousPtr != 0) {
     printf("%i\n", listPreviousPtr->value);
     listPreviousPtr = listPreviousPtr->previous;
