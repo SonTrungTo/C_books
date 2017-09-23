782e017e72642eb40dce8273acaebf00e7979f9d
SonTrungTo <son.trung.to@gmail.com>
2017-09-23 21:02:23 +0300

    double linked list, along with insertEntry and...
    removeEntry are written: More complex since the functions
    rely on two different types of pointers to position the entry.
    - removeEntry now deletes directly its argument.
    - Exercise 5+6,ch11 finished!

--- beginner/C_Kochan/ch11_exercises/doubleLinkedList.c
+++ beginner/C_Kochan/ch11_exercises/doubleLinkedList.c
@@ -10,66 +10,68 @@ struct entry {
 };
 
 // We make insertEntry add pointer to the new entry after the pointer to the old one
-// if next is true and before the pointer to the old one if otherwise.
-void insertEntry(struct entry *newEntry, struct entry *oldEntry, bool next) {
-  if (next) {
-    newEntry->next = oldEntry->next;
-    oldEntry->next = newEntry;
-  } else {
-    newEntry-> previous = oldEntry->previous;
-    oldEntry-> previous = newEntry;
+void insertEntry(struct entry *newEntry, struct entry *oldEntry) {
+    newEntry->previous       = oldEntry;
+    newEntry->next           = oldEntry->next;
+    oldEntry->next           = newEntry;
+    newEntry->next->previous = newEntry;
   }
-}
 
 void removeEntry(struct entry *listEntry) {
-  
+  listEntry->previous->next = listEntry->next; // This removes listEntry from the list.
+  listEntry->next->previous = listEntry->previous;
 }
 
 int main(void) {
-  struct entry n1, n2, n3, n2_3, *listNextPtr = &n1, *listPreviousPtr = &n3;
-  void insertEntry(struct entry *newEntry,struct entry *oldEntry,bool next);
+  struct entry n1, n2, n3, n2_3, upperDummyList, lowerDummyList;
+  struct entry *listNextPtr;
+  void insertEntry(struct entry *newEntry,struct entry *oldEntry);
+
+  upperDummyList.next     = &n1;
+  upperDummyList.previous = (struct entry *) 0;
+  listNextPtr = upperDummyList.next;
 
   n1.value    = 100;
   n1.next     = &n2;
-  n1.previous = (struct entry *) 0;
+  n1.previous = &upperDummyList;
 
   n2.value    = 200;
   n2.next     = &n3;
   n2.previous = &n1;
 
   n3.value    = 300;
-  n3.next     = (struct entry *) 0;
+  n3.next     = &lowerDummyList;
   n3.previous = &n2;
+
+  lowerDummyList.next       = (struct entry *) 0;
+  lowerDummyList.previous   = &n3;
 
   n2_3.value = 23579;
 
   printf("Next-order double linked list:\n");
-  while (listNextPtr != 0) {
+  while (listNextPtr->next != 0) {
     printf("%i\n", listNextPtr->value);
     listNextPtr = listNextPtr->next;
   }
 
-  printf("Previous-order double linked list:\n");
-  while (listPreviousPtr != 0) {
-    printf("%i\n", listPreviousPtr->value);
-    listPreviousPtr = listPreviousPtr->previous;
-  }
+  insertEntry(&n2_3,&n3);
+  listNextPtr = upperDummyList.next;
 
-  listNextPtr = &n1;
-  listPreviousPtr = &n3;
-  insertEntry(&n2_3,&n2,true);
-  insertEntry(&n2_3,&n2,false);
-
-  printf("Inserting n2_3 after element n2 gives:\n");
-  while (listNextPtr != 0) {
+  printf("Inserting n2_3 after element n3 gives:\n");
+  while (listNextPtr->next != 0) {
     printf("%i\n", listNextPtr->value);
     listNextPtr = listNextPtr->next;
   }
 
-  printf("Inserting n2_3 before element n2 gives:\n");
-  while (listPreviousPtr != 0) {
-    printf("%i\n", listPreviousPtr->value);
-    listPreviousPtr = listPreviousPtr->previous;
+  removeEntry(&n3);
+  removeEntry(&n2_3);
+  removeEntry(&n1);
+  listNextPtr     = upperDummyList.next;
+
+  printf("Removing n2_3, n3 and n1 from the list:\n");
+  while (listNextPtr->next != 0) {
+    printf("%i\n", listNextPtr->value);
+    listNextPtr = listNextPtr->next;
   }
 
   return 0;
