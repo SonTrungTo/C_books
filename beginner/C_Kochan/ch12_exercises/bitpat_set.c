c6806e9d30ad06467173e2f7f80fb96dc84a12b8
SonTrungTo <son.trung.to@gmail.com>
2017-11-11 18:19:45 +0200

    bitpat_set still writing
    - Some problems: the OR operator is not suitable for setting
    the bits.

--- beginner/C_Kochan/ch12_exercises/bitpat_set.c
+++ beginner/C_Kochan/ch12_exercises/bitpat_set.c
@@ -15,23 +15,9 @@ unsigned int int_size(void) {
   return count;
 }
 
-// bit_test function to return 1 bit number n.
-unsigned int bit_test(unsigned int value, int n_bit) {
-  unsigned int int_size(void), rightmostBitWanted;
-
-  if (n_bit < 0 || n_bit > (int_size() - 1)) {
-    printf("Re-check the second argument of bit_test. ");
-    return 2;
-  }
-
-  rightmostBitWanted = value >> (int_size() - 1 - n_bit);
-
-  return (rightmostBitWanted & 1);
-}
-
 // bitpat_set
 unsigned int bitpat_set(unsigned int *source, unsigned int value, int start, int size) {
-  unsigned int int_size(void);
+  unsigned int int_size(void), finalValue;
   int          upperLimit = start + size;
 
   if (size <= 0 || size > (int_size() - start)) {
@@ -44,5 +30,19 @@ unsigned int bitpat_set(unsigned int *source, unsigned int value, int start, int
     return 0;
   }
 
-  return *source;
+  finalValue = *source | (0 | value << int_size() - size) >> start;
+
+  return finalValue;
+}
+
+//Testing
+int main(void) {
+  unsigned int int_size(void);
+  unsigned int bitpat_set(unsigned int *source, unsigned int value, int start, int size);
+  unsigned int x = 0x55u;
+
+  printf("%u\n", bitpat_set(&x,0x55u,0,8));
+  printf("%u\n", bitpat_set(&x,0x55u,int_size() - 8,7)); // This is not good!
+
+  return 0;
 }
