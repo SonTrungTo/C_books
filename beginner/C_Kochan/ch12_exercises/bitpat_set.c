bf3f699ae07e3cbdba26d79ff447d34448d23fc9
SonTrungTo <son.trung.to@gmail.com>
2017-11-12 03:24:10 +0200

    Add some ideas for bitpat_set

--- beginner/C_Kochan/ch12_exercises/bitpat_set.c
+++ beginner/C_Kochan/ch12_exercises/bitpat_set.c
@@ -15,6 +15,26 @@ unsigned int int_size(void) {
   return count;
 }
 
+// Return n-th bit.
+unsigned int bit_test(unsigned int value, int n_bit) {
+  unsigned int int_size(void), rightmostBitWanted;
+
+  if (n_bit < 0 || n_bit > (int_size() - 1)) {
+    printf("Re-check the second argument of bit_test. ");
+    return 2;
+  }
+
+  rightmostBitWanted = value >> (int_size() - 1 - n_bit);
+
+  return (rightmostBitWanted & 1);
+}
+
+// Print the bits
+void printBits(unsigned int value) {
+  
+}
+
+
 // bitpat_set
 unsigned int bitpat_set(unsigned int *source, unsigned int value, int start, int size) {
   unsigned int int_size(void), finalValue;
