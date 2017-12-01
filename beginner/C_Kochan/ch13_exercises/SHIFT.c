c166b445d70e1e2c25c9e49d7f9c811de15d0d5e
SonTrungTo <son.trung.to@gmail.com>
2017-12-01 06:24:05 +0200

    Exercise 5 done! SHIFT written!

--- beginner/C_Kochan/ch13_exercises/SHIFT.c
+++ beginner/C_Kochan/ch13_exercises/SHIFT.c
@@ -1,3 +1,15 @@
 // Exercise 5: macro SHIFT to perform the shifting bits, as in bitShiftFunction.c
 #include <stdio.h>
-#define SHIFT(n,c) ( (c) > 0 ?) 
+#define printName(name) printf(#name" = %o\n", name);
+#define SHIFT(n,c)      ( (c) > 0 ? (n << c) : (n >> (-c)) )
+
+int main(void) {
+  unsigned int w1 = 0177777u, w2 = 0444u;
+
+  printName(SHIFT(w1,5));
+  printName(SHIFT(w1,-6));
+  printName(SHIFT(w2,0))
+  printName(SHIFT(SHIFT(w1,-3),3)); // net effect: w1 & ~7;
+
+  return 0;
+}
