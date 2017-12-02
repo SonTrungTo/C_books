3df668dbde774e17dd50b9395df8cb51c8104593
SonTrungTo <son.trung.to@gmail.com>
2017-12-02 19:07:52 +0200

    Exercise 8 done: IS_DIGIT and IS_SPECIAL

--- beginner/C_Kochan/ch13_exercises/MACRO_IS.c
+++ beginner/C_Kochan/ch13_exercises/MACRO_IS.c
@@ -1,10 +1,13 @@
 // Exercise 7: IS_ALPHABETIC gives a nonzero value if a character is an alphabetic character.
+// Exercise 8: IS_DIGIT and IS_SPECIAL. The point here is to use method of elimination.
 #include <stdio.h>
 #include <stdbool.h>
 #define printName(name)  printf(#name" = %i\n", name)
 #define IS_LOWER_CASE(c) ( (c) >= 'a' && (c) <= 'z' )
 #define IS_UPPER_CASE(C) ( (C) >= 'A' && (C) <= 'Z' )
 #define IS_ALPHABETIC(a) ( IS_LOWER_CASE(a) == IS_UPPER_CASE(a) ? 0 : 1)
+#define IS_DIGIT(d)      ( (d) >= '0' && (d) <= '9' )
+#define IS_SPECIAL(s)    ( IS_ALPHABETIC(s) == IS_DIGIT(s) ? 1 : 0 )
 
 // readLine is integrated from exercise 9, ch11.
 void readLine(char *buffer) {
@@ -42,6 +45,9 @@ int main(void) {
   for (int l = 0; l <= i; l++) {
     printf("l = %c; ", words[l]);
     printName(IS_ALPHABETIC(words[l]));
+    printName(IS_DIGIT(words[l]));
+    printName(IS_SPECIAL(words[l]));
+    printf("\n");
   }
 
   return 0;
