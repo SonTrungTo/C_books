2ee2c256b79afaac9aa3624c2effbb5644f95a1b
SonTrungTo <son.trung.to@gmail.com>
2017-12-02 11:52:22 +0200

    Exercise 6 done! IS_UPPER_CASE written!

---
+++ beginner/C_Kochan/ch13_exercises/IS_UPPER_CASE.c
@@ -0,0 +1,14 @@
+// Exercise 6: IS_UPPER_CASE gives a non-zero value if a character is an uppercase letter.
+#include <stdio.h>
+#define printName(name)  printf(#name" = %i\n", name);
+#define IS_UPPER_CASE(c) ( (c) >= 'A' && (c) <= 'Z' )
+
+int main(void) {
+  printName(IS_UPPER_CASE('A'));
+  printName(IS_UPPER_CASE('a'));
+  printName(IS_UPPER_CASE('+'));
+  printName(IS_UPPER_CASE('H'));
+  printName(IS_UPPER_CASE('h'));
+
+  return 0;
+}
