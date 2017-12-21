b2e21f7435ba59ca015bf9b35e4c75244947513a
SonTrungTo <son.trung.to@gmail.com>
2017-12-21 22:23:42 +0200

    Exercise 4, ch16, updated!

--- beginner/C_Kochan/ch16_exercises/mergeAlter.c
+++ beginner/C_Kochan/ch16_exercises/mergeAlter.c
@@ -1,1 +1,20 @@
-// Exercise 4: Program to merge lines alternatively.
+// Exercise 4: Program to merge lines alternatively from two different files.
+#include <stdio.h>
+#include <stdlib.h>
+
+void mergeLines(void) {
+  char file1Name[64], file2Name[64];
+  FILE *in1, *in2;
+  int  c;
+
+  // Name of files.
+  printf("", );
+}
+
+int main(void) {
+  void mergeLines(void);
+
+  mergeLines();
+
+  return 0;
+}
