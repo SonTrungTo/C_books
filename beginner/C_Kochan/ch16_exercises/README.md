a8bdf1e92e577d1dc3dcaf46d8b9e043d03d79bc
SonTrungTo <son.trung.to@gmail.com>
2017-12-22 23:50:01 +0200

    Exercise 4, ch16 finished!
    - Merge lines from two files alternately.

--- beginner/C_Kochan/ch16_exercises/README.md
+++ beginner/C_Kochan/ch16_exercises/README.md
@@ -2,3 +2,6 @@
 
 * copyFileUpper.c == Exercise 3: Program to copy one file to another,
 replacing all lowercase characters with their uppercase equivalents.
+* mergeAlter.c == Exercise 4: Program to merge lines alternately; if one file
+has less lines than the other, the remaining lines from the larger files
+are simply copied to stdout.
--- beginner/C_Kochan/ch16_exercises/mergeAlter.c
+++ beginner/C_Kochan/ch16_exercises/mergeAlter.c
@@ -5,13 +5,14 @@
 void mergeLines(void) {
   char file1Name[64], file2Name[64], buffer1[64], buffer2[64];
   FILE *in1, *in2;
-  int  c;
+  int  c1, c2;
 
   // Name of files.
   printf("Enter the name of the first file: ");
   scanf("%63s", file1Name);
   printf("Enter the name of the second file: ");
   scanf("%63s", file2Name);
+  printf("\n\n");
 
   // Test of opening files
   if ( (in1 = fopen(file1Name,"r")) == NULL ) {
@@ -25,17 +26,31 @@ void mergeLines(void) {
   }
 
   // Merging lines alternately between two files
-  // First testing how fgets works!
-  fgets(buffer1,64,in1);
-  fputs(buffer1,stdout);
-  fgets(buffer2,64,in2);
-  fputs(buffer2,stdout);
-  fgets(buffer1,64,in1);
-  fputs(buffer1,stdout);
+  while ( (c1 = getc(in1)) != EOF || (c2 = getc(in2)) != EOF ) {
+    if ( c1 == EOF ) {
+      putc(c2,stdout);
+      fgets(buffer2,64,in2);
+      fputs(buffer2,stdout);
+    } else if ( (c2 = getc(in2)) == EOF ) { // Because of short circuit evaluation!
+      putc(c1,stdout);
+      fgets(buffer1,64,in1);
+      fputs(buffer1,stdout);
+    } else {
+      putc(c1,stdout);
+      fgets(buffer1,64,in1);
+      fputs(buffer1,stdout);
+      putc(c2,stdout);
+      fgets(buffer2,64,in2);
+      fputs(buffer2,stdout);
+    }
+  }
 
   // Closing files
   fclose(in1);
   fclose(in2);
+
+  printf("\n\n");
+  printf("Files have been successfully alternately merged!\n");
 
 }
 
