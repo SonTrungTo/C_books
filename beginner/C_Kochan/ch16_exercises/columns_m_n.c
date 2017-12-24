32ea5f184c44cfae7486fe330bce50b40c26b7be
SonTrungTo <son.trung.to@gmail.com>
2017-12-24 16:50:02 +0200

    Exercise 5, ch16 solved!
    - some problems I still don't understand!

--- beginner/C_Kochan/ch16_exercises/columns_m_n.c
+++ beginner/C_Kochan/ch16_exercises/columns_m_n.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 void columns_m_n(void) {
-  char  fileName[64], lineOfText[64], buffer[64];
+  char  fileName[64], lineOfText[81], buffer[64];
   FILE  *in;
   int   columnValue, charLength, i;
 
@@ -15,20 +15,34 @@ void columns_m_n(void) {
   scanf("%i", &columnValue);
   printf("Enter the character length: ");
   scanf("%i", &charLength);
+  printf("\n\n");
 
   // reading file and checking value of m and n
   if ( (in = fopen(fileName,"r")) == NULL ) {
     fprintf(stderr,"Cannot open %s for reading.\n", fileName);
     exit(EXIT_FAILURE);
   }
-
-  while ( (fgets(lineOfText,64,in)) != NULL ) {
-    for (i = 0; i < charLength; i++) {
-      lineOfText[(columnValue - 1) + i] = buffer[i];
-    }
-    buffer[i + 1] = '\n';
-    fputs(lineOfText,stdout);
+  if (columnValue < 1 || columnValue > 64) {
+    fprintf(stderr, "Bad column value. (m is bad)\n");
+    exit(EXIT_FAILURE);
   }
+  if (charLength < 1) {
+    fprintf(stderr, "Bad character length. (n is bad)\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Write columns m through n of each line
+  while ( (fgets(lineOfText,81,in)) != NULL ) { // Very weird! if the lineOfText is too small(64), it can cause unpredictable results!
+    for (i = 0; i < charLength; i++) {
+      buffer[i] = lineOfText[(columnValue - 1) + i];
+    }
+    if (buffer[i] != '\0' || buffer[i] != '\n')
+      buffer[i] = '\n';
+    fputs(buffer,stdout);
+  }
+
+  // Closing file
+  fclose(in);
 
   printf("\n\n");
   printf("Output has been displayed at m through n.\n");
