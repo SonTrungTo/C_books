0fd25f74ac0dfeb8b9682b889baeaa879dc078e3
SonTrungTo <son.trung.to@gmail.com>
2017-12-27 09:05:20 +0200

    Example 18.3: Redefined preprocessor for the debugger.

---
+++ beginner/C_Kochan/ch18/p18-3.c
@@ -0,0 +1,28 @@
+// Example 18.3: Change the way preprocessor is used in p18-2.c
+#include <stdio.h>
+#include <stdlib.h>
+#define DEBUG(ftm,...) fprintf(stderr, ftm, __VA_ARGS__)
+
+int process(int i1, int i2) {
+  int val;
+
+  DEBUG("process (%i, %i)\n", i1, i2);
+  val = i1 * i2;
+  DEBUG("return %i\n",val);
+
+  return val;
+}
+
+int main(int argc, char const *argv[]) {
+  int arg1 = 0, arg2 = 0;
+
+  if (argc > 1)
+    arg1 = atoi(argv[1]);
+  if (argc == 3)
+    arg2 = atoi(argv[2]);
+  DEBUG("processed %i arguments\n", argc - 1);
+  DEBUG("arg1 = %i, arg2 = %i\n", arg1, arg2);
+  printf("%i\n", process(arg1, arg2));
+
+  return 0;
+}
