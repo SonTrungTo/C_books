a3ee82f2818505d94261ee980a3040ef922e4932
SonTrungTo <son.trung.to@gmail.com>
2018-02-25 07:22:44 +0200

    cnt_letters.c written!

--- beginner/BoC/cnt_letters.c
+++ beginner/BoC/cnt_letters.c
@@ -13,7 +13,22 @@ int main(int argc, char const *argv[]) {
       "The results will be written in outfile.");
     exit(1);
   }
-  
+  ifp = fopen(argv[1], "r");
+  ofp = fopen(argv[2], "w");
+  for (i = 0; i < 26; i++) { // Initialize arrays to 0.
+    letter[i] = 0;
+  }
+  while ( (c = getc(ifp)) != EOF ) { // Find uppercase letters
+    if (c >= 'A' && c <= 'Z') {
+      ++letter[c - 'A'];
+    }
+  }
+  for (i = 0; i < 26; i++) {      // print results
+    if (i % 6 == 0)
+      putc('\n', ofp);
+    fprintf(ofp, "%c:%5d     ", 'A' + i, letter[i]);
+  }
+  putc('\n', ofp);
 
   return 0;
 }
