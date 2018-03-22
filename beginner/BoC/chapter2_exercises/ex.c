fe8796839bf0a2736c5e6308a77ad2a7e92e5c92
SonTrungTo <son.trung.to@gmail.com>
2018-03-22 21:31:41 +0200

    ex1-3 done!

---
+++ beginner/BoC/chapter2_exercises/ex.c
@@ -0,0 +1,13 @@
+/*
+*   Exercise 1: Is main a keyword? Explain. NO! Keywords cannot be modified in anyway,
+* while main() is an identifier for a function.
+*   Exercise 2: int, float, const, char, double, return.
+*   Exercise 3:
+*   - Keywords:         int, struct
+*   - Identifiers:      main, <function name>, <variable name>
+*   - Constant:         3, -3.5, 'c', "c"
+    - String Constant:  "Dies Irae", " ", ""
+    - Operator:         main() ,+. -, *, +=
+    - Punctuator:       (1 + 2), int a, b, c;
+    
+*/
