df2d1bb3b0ef331b29e031d134ff6fe5ced51683
SonTrungTo <son.trung.to@gmail.com>
2018-03-23 02:19:55 +0200

    ex: 1-8 done!

--- beginner/BoC/chapter2_exercises/ex.c
+++ beginner/BoC/chapter2_exercises/ex.c
@@ -9,5 +9,9 @@
     - String Constant:  "Dies Irae", " ", ""
     - Operator:         main() ,+. -, *, +=
     - Punctuator:       (1 + 2), int a, b, c;
-    
+    Exercise 4: Using definition of identifier,
+    3id       1_i_am      one_i_aren't      me_to-2     00_go     star*it
+    Exercise 5:  Too easy.
+    Exercise 6: ++a, a++, a += 1, a + 1.
+    Exercise 7, 8: Too easy.
 */
