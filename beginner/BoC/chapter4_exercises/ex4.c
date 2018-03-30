0f616f27bb65e88d883d2b44fde67965a9fb525e
SonTrungTo <son.trung.to@gmail.com>
2018-03-30 16:22:42 +0300

    ex4 done

--- beginner/BoC/chapter4_exercises/ex4.c
+++ beginner/BoC/chapter4_exercises/ex4.c
@@ -1,1 +1,15 @@
-/*  */
+/* Count the number of times the first three letters of the alphabet appear */
+#include <stdio.h>
+
+int main(void) {
+  int    c, alpha_count = 0;
+
+  while ( (c = getchar()) != EOF) {
+    if ( c >= 'a' && c <= 'c' || c >= 'A' && c <= 'C')
+      ++alpha_count;
+  }
+
+  printf("The file contains %d first-three-letter letters of the alphabet.\n", alpha_count);
+
+  return 0;
+}
