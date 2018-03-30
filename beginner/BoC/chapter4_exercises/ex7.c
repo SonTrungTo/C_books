a54a74ece9305fa24f7ea2bae0e85c844a94a7c6
SonTrungTo <son.trung.to@gmail.com>
2018-03-31 00:04:11 +0300

    ex7 solved!

---
+++ beginner/BoC/chapter4_exercises/ex7.c
@@ -0,0 +1,24 @@
+/* In the first piece of the code, the loop while is infinite.
+          second piece of the code, the if-statement is always TRUE.
+ */
+//#include <stdio.h>
+
+// int main(void) {
+//   int     i;
+//
+//   while (i = 2) {
+//     printf("Some even numbers: %d %d %d\n", i, i + 2, i + 4);
+//     i = 0;
+//   }
+//
+//   return 0;
+//}
+
+// int main(void) {
+//   int     i;
+//
+//   if (i = 2)
+//     printf("Some even numbers: %d %d %d\n", i, i + 2, i + 4);
+//
+//   return 0;
+// }
