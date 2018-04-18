c06f04d8a7f0757786af1b4baf072c95a7c3b33d
SonTrungTo <son.trung.to@gmail.com>
2018-04-18 17:50:00 +0300

    kepler done!

--- beginner/BoC/kepler/bisection.c
+++ beginner/BoC/kepler/bisection.c
@@ -4,12 +4,10 @@ dbl bisection(dbl f(dbl x), dbl a, dbl b) {
   dbl   m = (a + b) / 2;
 
   ++cnt;
-  if (/* condition */) {
-    /* code */
-  } else if (/* condition */) {
-    /* code */
-  } else {
-    /* code */
-  }
-
+  if (f(m) == 0 || fabs(b - a) < eps)
+    return m;
+  else if (f(a) * f(m) <= 0)
+    return bisection(f, a, m);
+  else
+    return bisection(f, m, b);
 }
---
+++ beginner/BoC/kepler/kepler.c
@@ -0,0 +1,5 @@
+#include "kepler.h"
+
+dbl kepler(dbl x) {
+  return (x - e*sin(x) - m);
+}
