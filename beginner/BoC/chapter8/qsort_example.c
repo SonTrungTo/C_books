89ab8a9dcb917c6b4776b258b6d6ac4a7493cf64
SonTrungTo <son.trung.to@gmail.com>
2018-07-15 17:58:48 +0300

    quicksort finished!

--- beginner/BoC/chapter8/qsort_example.c
+++ beginner/BoC/chapter8/qsort_example.c
@@ -1,4 +1,5 @@
 /* Quicksort! Pointer version with macros. */
+#include <stdio.h>
 
 #define   swap(x, y)   { int t;  t = x; x = y; y = t; }
 #define   order(x, y)  if( x > y )  swap(x, y)
@@ -12,4 +13,51 @@ static  int     *partition(int  *left, int  *right, int  pivot);
 
 void quicksort(int  *left, int  *right) {
   int     *p, pivot;
+
+  if (find_pivot(left, right, &pivot) == yes) {
+    p = partition(left, right, pivot);
+    quicksort(left, p - 1);
+    quicksort(p, right);
+  }
+}
+
+static  yes_no  find_pivot(int  *left, int  *right, int  *pivot_ptr) {
+  int   a, b, c, *p;
+
+  a = *left;                            /* left value */
+  b = *(left + (right - left) / 2);     /* middle value */
+  c = *right;                           /* right value */
+  o3(a, b, c);                          /* re-ordering */
+
+  if (a < b) {
+    *pivot_ptr = b;
+    return  yes;
+  }
+  if (b < c) {
+    *pivot_ptr = c;
+    return  yes;
+  }
+  for (p = left + 1; p <= right; p++)
+    if (*p != *left) {
+      *pivot_ptr = (*p > *left) ? *p : *left;
+      return  yes;
+    }
+
+  return  no;
+}
+
+static  int    *partition(int  *left, int  *right, int  pivot) {
+  while (left <= right) {
+    while (*left < pivot)
+      ++left;
+    while (*right >= pivot)
+      --right;
+    if (left < right) {
+      swap(*left, *right);
+      ++left;
+      --right;
+    }
+  }
+
+  return  left;
 }
