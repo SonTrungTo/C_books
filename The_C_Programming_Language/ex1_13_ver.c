54363a33daa745bfb6478ea68627e54ae6a90f9e
SonTrungTo <son.trung.to@gmail.com>
2018-07-23 13:35:47 +0300

    ex1_13 completely solved! YAY!

--- The_C_Programming_Language/ex1_13_ver.c
+++ The_C_Programming_Language/ex1_13_ver.c
@@ -6,9 +6,10 @@
 #define     MAX_HIST    64
 
 int main(void) {
-  int    c, i, state, legnth;
+  int    c, i, j, state, legnth;
   int    max_words;
   int    wl[MAX_LENGTH];
+  int    wn[MAX_LENGTH];          /* This array is used for label */
   int    wc;
   int    overflow;
 
@@ -29,10 +30,55 @@ int main(void) {
       wc = 0;
     }
     else if (state == OUT) {
-      
+      state = IN;
+      wc = 1;
     }
     else
+      ++wc;
   }
+
+  for (i = 0; i < MAX_LENGTH; i++)
+    wn[i] = wl[i];
+
+  for (i = 1; i < MAX_LENGTH; i++)
+    if (max_words < wl[i])
+      max_words = wl[i];
+
+  if (max_words > MAX_HIST)
+    printf("WARNING: Word counters (%d) exceed maximum length of histogram (%d).\n",
+  max_words, MAX_HIST);
+
+  for (i = MAX_HIST; i > 0; i--) {
+    if (i == max_words) {
+      for (j = 1; j < MAX_LENGTH; j++)
+        if ( wl[j] > 0 && wl[j] == max_words ) {
+          --wl[j];
+          printf(" * ");
+        }
+        else if (wl[j] > 0)
+          printf("   ");
+      --max_words;
+    }
+    putchar('\n');
+  }
+
+  putchar('\n');
+
+  for (i = 1; i < MAX_LENGTH; i++)
+    if (wn[i] > 0)
+      printf("%2d ", wn[i]);
+  putchar('\n');
+  for (i = 1; i < MAX_LENGTH; i++)
+    if (wn[i] > 0)
+      printf("%2s ", "--");
+  putchar('\n');
+  for (i = 1; i < MAX_LENGTH; i++)
+    if (wn[i] > 0)
+      printf("%2d ", i);
+  putchar('\n');
+
+  if (overflow > 0)
+    printf("There are %d overflown words that are greater than %d\n", overflow, MAX_LENGTH);
 
   return 0;
 }
