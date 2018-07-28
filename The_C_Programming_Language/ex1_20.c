fee56c05ec7ce96f72e1ca03ace8145449de49d8
SonTrungTo <son.trung.to@gmail.com>
2018-07-28 18:56:01 +0300

    ex1_20: First draft!

--- The_C_Programming_Language/ex1_20.c
+++ The_C_Programming_Language/ex1_20.c
@@ -1,1 +1,48 @@
 #include <stdio.h>
+#define   MAXLINE   100
+#define   N_COLS    2
+
+int  getLine(char line[], int size);
+void detab(char line[]);
+
+int main(void) {
+  int    len;
+  int    line[MAXLINE];
+
+  while ((len = getLine(line, MAXLINE)) > 0) {
+    detab(line);
+    printf("%s", line);
+  }
+  return 0;
+}
+
+int getLine(char s[], int lim) {
+  int    c, i, j;
+
+  j = 0;
+  for (i = 0; (c = getchar()) != EOF && c != '\n'; i++)
+    if (i < lim - 2) {
+      s[j] = c;
+      ++j;
+    }
+
+  if (c == '\n') {
+    s[j] = c;
+    ++j;
+    ++i;
+  }
+
+  s[j] = '\0';
+
+  return i;
+}
+
+void detab(char s[]) {
+  int    i;
+
+  for (i = 0; s[i] != '\n'; i++) {
+    if (s[i] == '\t') {
+      /* code */
+    }
+  }
+}
