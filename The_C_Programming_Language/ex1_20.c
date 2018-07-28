e36004c737490eb77a892a564076296dea79127d
SonTrungTo <son.trung.to@gmail.com>
2018-07-28 19:56:13 +0300

    ex1_20 solved!

--- The_C_Programming_Language/ex1_20.c
+++ The_C_Programming_Language/ex1_20.c
@@ -1,48 +1,28 @@
 #include <stdio.h>
-#define   MAXLINE   100
-#define   N_COLS    2
+#define  TAB_SIZE     5
 
-int  getLine(char line[], int size);
-void detab(char line[]);
 
 int main(void) {
-  int    len;
-  int    line[MAXLINE];
+  int    c, nb, pos;
 
-  while ((len = getLine(line, MAXLINE)) > 0) {
-    detab(line);
-    printf("%s", line);
+  nb  = 0;          /* Number of blank spaces */
+  pos = 1;          /* Position counter       */
+
+  while ((c = getchar()) != EOF) {
+    if (c == '\t') {
+      nb = TAB_SIZE - (pos - 1) % TAB_SIZE;
+      while (nb-- > 0) {
+        putchar('#');
+        ++pos;
+      }
+    } else if (c == '\n') {
+      putchar(c);
+      pos = 1;
+    } else {
+      putchar(c);
+      ++pos;
+    }
   }
+
   return 0;
-}
-
-int getLine(char s[], int lim) {
-  int    c, i, j;
-
-  j = 0;
-  for (i = 0; (c = getchar()) != EOF && c != '\n'; i++)
-    if (i < lim - 2) {
-      s[j] = c;
-      ++j;
-    }
-
-  if (c == '\n') {
-    s[j] = c;
-    ++j;
-    ++i;
-  }
-
-  s[j] = '\0';
-
-  return i;
-}
-
-void detab(char s[]) {
-  int    i;
-
-  for (i = 0; s[i] != '\n'; i++) {
-    if (s[i] == '\t') {
-      /* code */
-    }
-  }
 }
