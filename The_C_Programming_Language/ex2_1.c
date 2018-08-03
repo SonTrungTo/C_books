82eec2295eb4460daecc7941d2e849254f4afa07
SonTrungTo <son.trung.to@gmail.com>
2018-08-03 07:48:57 +0300

    ex2_1: Printing max,min of signed, unsigned char

--- The_C_Programming_Language/ex2_1.c
+++ The_C_Programming_Language/ex2_1.c
@@ -4,6 +4,8 @@
 
 int main(void) {
   printf("The range of char is from %d to %d\n", CHAR_MIN, CHAR_MAX);
+  printf("The range of signed char is from %d to %d\n", SCHAR_MIN, SCHAR_MAX);
+  printf("The range of unsigned char is from %d to %d\n", 0, UCHAR_MAX);
 
   return 0;
 }
