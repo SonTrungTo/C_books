ef3646661624feeea3204fa3b237cf3fbf9ed090
SonTrungTo <son.trung.to@gmail.com>
2018-08-04 13:04:21 +0300

    ex2_1 done!

--- The_C_Programming_Language/ex2_1.c
+++ The_C_Programming_Language/ex2_1.c
@@ -3,6 +3,7 @@
 #include <float.h>
 
 int main(void) {
+  /* By printing appropriate values from headers */
   printf("The range of char is from %d to %d\n", CHAR_MIN, CHAR_MAX);
   printf("The range of signed char is from %d to %d\n", SCHAR_MIN, SCHAR_MAX);
   printf("The range of unsigned char is from %d to %d\n", 0, UCHAR_MAX);
@@ -14,6 +15,16 @@ int main(void) {
   printf("min(ulong) = %d, max(ulong) = %lu\n", 0, ULONG_MAX);
   printf("min(float) = %f, max(float) = %f\n", FLT_MIN, FLT_MAX);
   printf("min(double) = %lf, max(double) = %lf\n", DBL_MIN, DBL_MAX);
+  /* By direct computation */
+  printf("SCHAR_MIN = %d, SCHAR_MAX = %d\n", -(char)((unsigned char) ~0 >> 1),
+(char)((unsigned char) ~0 >> 1));
+  printf("UCHAR_MIN = 0 ,UCHAR_MAX = %d\n", (unsigned char) ~0);
+  printf("SHRT_MIN = %d, SHRT_MAX = %d\n", -(short)((unsigned short) ~0 >> 1),
+(short)((unsigned short) ~0 >> 1));
+  printf("USHORT_MIN = 0, USHORT_MAX = %d\n", (unsigned short) ~0);
+  printf("INT_MIN = %d, INT_MAX = %d\n", -(int)((unsigned int) ~0 >> 1),
+(int)((unsigned int) ~0 >> 1));
+/* etc... */
 
   return 0;
 }
