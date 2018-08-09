c0333db0a6863b57a3ab4e5a9c62321e9324a748
SonTrungTo <son.trung.to@gmail.com>
2018-08-09 04:19:26 +0300

    ex2_4 done!

--- The_C_Programming_Language/ex2_4.c
+++ The_C_Programming_Language/ex2_4.c
@@ -8,7 +8,7 @@ void   squeeze(char s1[], char s2[]);
 
 int main(void) {
   char  s1[] = "Finland is a great!";
-  char  s2[] = "I have lived in Fin";
+  char  s2[] = "I want to buy her flowers from Kaarina.";
 
   printf("Before--\n");
   printf("%s\n", s1);
@@ -22,10 +22,18 @@ int main(void) {
 }
 
 void squeeze(char s1[], char s2[]) {
-  int    i, j, noMatch;
+  int    i, j, k, noMatch;
 
   noMatch = YES;
-  for (i = 0; s1[i] != '\0'; i++) {
+  for (i = k = 0; s1[i] != '\0'; i++) {
+    for (j = 0; noMatch == YES && s2[j] != '\0'; j++)
+      if (s1[i] == s2[j])
+        noMatch = NO;
 
+    if (noMatch == YES)
+      s1[k++] = s1[i];
+    noMatch = YES;
   }
+
+  s1[k] = '\0';
 }
