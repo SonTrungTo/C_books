c66e134cb6bbbf698fb9608049346416b8f69f0a
SonTrungTo <son.trung.to@gmail.com>
2018-08-12 10:24:38 +0300

    ex2_6 done!

--- The_C_Programming_Language/ex2_6.c
+++ The_C_Programming_Language/ex2_6.c
@@ -9,17 +9,16 @@ int main(void) {
   unsigned a = 051, b = 015;
 
   printf("%u\n", setbits(x, 3, 3, y)); /* Expect: 39 */
-  printf("%u\n", setbits(x, 3, 2, y)); /* Expect: 37 */
+  printf("%u\n", setbits(a, 3, 2, b)); /* Expect: 37 */
   printf("%u\n", setbits2(x, 3, 3, y));
-  printf("%u\n", setbits2(x, 3, 2, y));
+  printf("%u\n", setbits2(a, 3, 2, b));
   return 0;
 }
 
-/* I do not know where I got wrong! */
 unsigned setbits(unsigned x, int p, int n, unsigned y) {
   return ((((x >> (p+1-n)) & (~0 << n)) | (y & ~(~0 << n))) << (p+1-n) | (x & ~(~0 << (p+1-n))));
 }
 
 unsigned setbits2(unsigned x, int p, int n, unsigned y) {
-  return  ;
+  return  x & ~(~(~0 << n) << (p+1-n)) | (y & ~(~0 << n)) << (p+1-n);
 }
