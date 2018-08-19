c8218ba35686b5d45c61ba6e6a45018c22b94786
SonTrungTo <son.trung.to@gmail.com>
2018-08-19 16:25:50 +0300

    ex3_2 done, tested!

--- The_C_Programming_Language/ex3_2.c
+++ The_C_Programming_Language/ex3_2.c
@@ -58,5 +58,27 @@ void escape(char s[], char t[]) {
 }
 
 void unescape(char s[], char t[]) {
-  
+  int   i, j;
+
+  for (i = j = 0; t[i] != '\0'; i++)
+    switch (t[i]) {
+      case '\\':
+        switch (t[++i]) {
+          case 'n':
+            s[j++] = '\n';
+            break;
+          case 't':
+            s[j++] = '\t';
+            break;
+          default:
+            s[j++] = '\\';
+            s[j++] = t[i];
+            break;
+        }
+        break;
+    default:
+      s[j++] = t[i];
+      break;
+    }
+  s[j] = '\0';
 }
