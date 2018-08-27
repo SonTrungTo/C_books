18fd3cfdf3f88ff49b3014489d1b681623db4678
SonTrungTo <son.trung.to@gmail.com>
2018-08-27 22:51:06 +0300

    ex3_5 in process

---
+++ The_C_Programming_Language/ex3_5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#define     BIGVALUE    (int) pow(2, sizeof(int) * 8 - 1);
+#define     abs(x)      ((x) < 0 ? -(x) : (x))
+
+void        itob(int n, char s[], int b);
+void        reverse(char s[]);
+
+int main(void) {
+  
+
+  return 0;
+}
