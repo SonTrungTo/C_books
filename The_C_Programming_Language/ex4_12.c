e9e33a59cbfc9c6844f35eb0b90a47ca2dd43ee6
SonTrungTo <son.trung.to@gmail.com>
2018-09-27 12:25:33 +0300

    ex4_12: itoa, recursive version.

---
+++ The_C_Programming_Language/ex4_12.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define     MAXSIZE     100
+#define     abs(x)      ((x) < 0 ? -(x) : (x))
+
+/* itoa, recursive version, works for the largest negative integer */
+void  itoa(int, char []);
+
+void itoa(int n, char s[]) {
+  static int    sign, i;
+
+  i = 0;
+  sign = 1;
+  if (n < 0 && sign) {
+    s[i++] = '-';
+    sign = 0;
+  }
+  if (n / 10)
+    itoa(n / 10, s);
+  s[i++] = abs(n % 10) + '0';
+}
+
+int main(void) {
+  char   s[MAXSIZE];
+  int    n;
+
+  printf("Enter n: ");
+  scanf("%i", &n);
+  itoa(n, s);
+  printf("String s = %s\n\n", s);
+
+  return 0;
+}
