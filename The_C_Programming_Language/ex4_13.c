a2978baef205a7003c3d78a5378a91a6a7bd5ced
SonTrungTo <son.trung.to@gmail.com>
2018-10-02 22:14:02 +0300

    ex4_13: can't figure it out...

--- The_C_Programming_Language/ex4_13.c
+++ The_C_Programming_Language/ex4_13.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 #define     MAXSIZE     100
 
 void   reverse(char []);
 
 void reverse(char s[]) {
-  
+  static int    i, len;
+
+  len = strlen(s);
+  i   = 0;
+  if (--len > 0)
+    reverse(s);
+  s[i++] = s;
 }
