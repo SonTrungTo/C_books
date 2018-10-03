76bc2f192d89bafe7db1f6bb84f4e08423962e0c
SonTrungTo <son.trung.to@gmail.com>
2018-10-03 09:50:15 +0300

    ex4_13 finished! But it's not my own solution!

--- The_C_Programming_Language/ex4_13.c
+++ The_C_Programming_Language/ex4_13.c
@@ -1,3 +1,4 @@
+/* Credits go to the solution manual. I can't solve this problem! */
 #include <stdio.h>
 #include <string.h>
 
@@ -6,11 +7,29 @@
 void   reverse(char []);
 
 void reverse(char s[]) {
-  static int    i, len;
+  void    reverser(char s[], int i, int len);
 
-  len = strlen(s);
-  i   = 0;
-  if (--len > 0)
-    reverse(s);
-  s[i++] = s;
+  reverser(s, 0, strlen(s));
+}
+
+void reverser(char s[], int i, int len) {
+  int     j, c;
+
+  j = len - (i + 1);                    /* the right index of a string */
+  if (i < j) {
+    c = s[i];
+    s[i] = s[j];
+    s[j] = c;
+    reverser(s, ++i, len);
+  }
+}
+
+int main(void) {
+  char   s[] = "abcde";
+
+  printf("Before: %s\n", s);
+  reverse(s);
+  printf("After : %s\n", s);
+
+  return 0;
 }
