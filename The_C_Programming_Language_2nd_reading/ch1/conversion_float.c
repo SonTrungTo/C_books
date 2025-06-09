cdfb95c37b3e64c549e2b73f44ed2fb6904f1ce2
SonTrungTo <son.trung.to@gmail.com>
2025-06-09 22:04:49 +0700

    fix: finish conversion_float.c

--- The_C_Programming_Language_2nd_reading/ch1/conversion_float.c
+++ The_C_Programming_Language_2nd_reading/ch1/conversion_float.c
@@ -6,4 +6,26 @@
 int main()
 {
     float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;          /* lower limit of temperature table */
+    upper = 300;        /* upper limit */
+    step = 20;          /* step size */
+
+    fahr = lower;
+    while (fahr <= upper)
+    {
+        celsius = (5.0/9.0) * (fahr-32.0);          /* Note the arithmetic expression conversion: int -> float */
+        printf("%3.0f %6.1f\n", fahr, celsius);     /* 3 characters wide, no decimal point, no fraction digits  6 characters wide, with 1 digit after the decimal point */
+        /* %f print the number as floating point */
+        /* %.2f print as floating point, 2 chatacters after decimal point */
+        /*  
+            %o: octal;
+            %x: hexadecimal;
+            %c: character;
+            %s: character string;
+            %%: % itself;
+        */
+        fahr = fahr + step;
+    }    
 }
