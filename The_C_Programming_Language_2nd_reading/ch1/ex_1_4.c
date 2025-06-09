cf2b8f67f0e7088274b1540b2f88fc5f68c963bb
SonTrungTo <son.trung.to@gmail.com>
2025-06-09 22:47:49 +0700

    fix: 1_4 in process

---
+++ The_C_Programming_Language_2nd_reading/ch1/ex_1_4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* Convert Celsius to Fahrenheit */
+int main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;    
+}
