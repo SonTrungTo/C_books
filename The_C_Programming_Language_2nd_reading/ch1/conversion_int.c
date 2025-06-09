7456fa7985b4d7b15b3c92d40457e85db5d3c0d6
SonTrungTo <son.trung.to@gmail.com>
2025-06-09 07:14:41 +0700

    fix: finish conversion_int.c

--- The_C_Programming_Language_2nd_reading/ch1/conversion_int.c
+++ The_C_Programming_Language_2nd_reading/ch1/conversion_int.c
@@ -7,5 +7,15 @@ int main()
     int fahr, celsius;
     int lower, upper, steps;
 
-    lower = 0; /* lower limit of temperature table */
+    lower = 0;      /* lower limit of temperature table */
+    upper = 300;    /* upper limit */
+    steps = 20;     /* step size */
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = 5 * (fahr-32) / 9; /* expression, integer truncates */
+        // printf("%d\t%d\n", fahr, celsius);
+        printf("%3d %6d\n", fahr, celsius); /* 3 digits wide for first variable, 6 digits wide for the next */
+        fahr = fahr + steps;
+    }
 }
