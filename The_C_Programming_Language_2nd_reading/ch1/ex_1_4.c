2c5549ad3c577ac7431730c084f7bbc3037edc5e
SonTrungTo <son.trung.to@gmail.com>
2025-06-10 03:07:33 +0700

    fix: complete 1_4

--- The_C_Programming_Language_2nd_reading/ch1/ex_1_4.c
+++ The_C_Programming_Language_2nd_reading/ch1/ex_1_4.c
@@ -3,10 +3,18 @@
 /* Convert Celsius to Fahrenheit */
 int main()
 {
-    float fahr, celsius;
-    int lower, upper, step;
+    float fahr;
+    int celsius, lower, upper, step;
 
     lower = 0;
     upper = 300;
-    step = 20;    
+    step = 20;
+
+    celsius = lower;
+    while (celsius <= upper)
+    {
+        fahr = (9.0/5.0) * celsius + 32.0;
+        printf("%3d %6.1f\n", celsius, fahr);
+        celsius = celsius + step;
+    }    
 }
