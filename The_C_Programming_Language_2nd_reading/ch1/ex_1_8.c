e555f52a08e2b0e11218088ac093accd34da66b1
SonTrungTo <son.trung.to@gmail.com>
2025-06-13 12:20:39 +0700

    fix: finish 1_8

--- The_C_Programming_Language_2nd_reading/ch1/ex_1_8.c
+++ The_C_Programming_Language_2nd_reading/ch1/ex_1_8.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 
+#define BLANK_STR   "(blank)"
+#define TABS_STR    "(tab)"
+#define NEWLINE_STR "(newline)"
+
 /* count blanks, tabs and newlines */
 int main()
 {
+    int c;
     int bc, tc, nc;
+
+    for (bc=0, tc=0, nc=0; (c = getchar()) != EOF;)
+    {
+       putchar(c);
+       if (c == ' ') {
+        ++bc;
+        printf(BLANK_STR);
+       }
+       if (c == '\t')
+       {
+        ++tc;
+        printf(TABS_STR);
+       }
+       if (c == '\n')
+       {
+        ++nc;
+        printf(NEWLINE_STR);
+       }
+    }
+    printf("\n\n");
+    printf("blanks: %d, tabs: %d, newlines: %d", bc, tc, nc);
+    printf("\n\n");
 }
