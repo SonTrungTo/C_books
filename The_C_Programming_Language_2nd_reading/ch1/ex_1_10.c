51ac8dff1123ede5b9edfd19d93ac3a9dcf71ddf
SonTrungTo <son.trung.to@gmail.com>
2025-06-14 10:28:23 +0700

    fix: solve 1_10

--- The_C_Programming_Language_2nd_reading/ch1/ex_1_10.c
+++ The_C_Programming_Language_2nd_reading/ch1/ex_1_10.c
@@ -1,3 +1,30 @@
 #include <stdio.h>
 
-/* Replacing tabs */
+/* Copying input to output, replacing each
+* tab       -> \t
+* backspace -> \b
+* backslash -> \\
+*/
+int main()
+{
+    int c, nt, nb, ns;
+    for (nt = 0, nb = 0, ns = 0; (c = getchar()) != EOF; nt = 0, nb = 0, ns = 0)
+    {
+        if (c == '\t') {
+            putchar('\\');
+            putchar('t');
+            ++nt;
+        }
+        if (c == '\b') {
+            putchar('\\');
+            putchar('b');
+            ++nb;
+        }
+        if (c == '\\') {
+            putchar('\\');
+            putchar('\\');
+            ++ns;
+        }       
+        if (nt == 0 && nb == 0 && ns == 0) putchar(c);
+    }
+}
