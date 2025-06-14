a0237b2829e6148c8e8b8862d76d1002fdadd230
SonTrungTo <son.trung.to@gmail.com>
2025-06-15 04:55:08 +0700

    fix: add word count program

--- The_C_Programming_Language_2nd_reading/ch1/wc.c
+++ The_C_Programming_Language_2nd_reading/ch1/wc.c
@@ -1,3 +1,24 @@
 #include <stdio.h>
 
+#define     IN      1 /* inside a word */
+#define    OUT      0 /* outside a word */
+
 /* count lines, words, and characters in input */
+int main()
+{
+    int c, nl, nw, nc, state;
+
+    nl = nw = nc = 0;
+    state = OUT;
+    while ((c = getchar()) != EOF) {
+        ++nc;
+        if (c == '\n') ++nl;
+        if (c == ' ' || c == '\t' || c == '\n')
+            state = OUT;
+        else if (state == OUT) {
+            state == IN;
+            ++nw;
+        }
+    }
+    printf("%d %d %d\n", nl, nw, nc);
+}
