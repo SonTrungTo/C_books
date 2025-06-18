ce04980f4e497ebfb3b3ceb23c084865a90376bf
SonTrungTo <son.trung.to@gmail.com>
2025-06-18 16:33:51 +0700

    fix: start 1_14

---
+++ The_C_Programming_Language_2nd_reading/ch1/ex_1_14.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define NCHAR_SIZE 26 /* Size of all alphabet characters in English */
+
+/* Print a histogram of the frequencies of different characters */
+int main()
+{
+    
+}
