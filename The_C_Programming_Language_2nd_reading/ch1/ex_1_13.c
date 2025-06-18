e84f2fd2c1985b1f5fdd91c4d4c2835a9f404b45
SonTrungTo <son.trung.to@gmail.com>
2025-06-18 08:42:55 +0700

    fix: prepare for vertical orientation

--- The_C_Programming_Language_2nd_reading/ch1/ex_1_13.c
+++ The_C_Programming_Language_2nd_reading/ch1/ex_1_13.c
@@ -8,9 +8,10 @@
 /* Print histogram of the lengths of words */
 int main()
 {
-    int c, state, wlength;
+    int c, state, threshold, wlength, ncat;
     int nlength[SIZE];
 
+    threshold = 0;
     wlength = 0;
     for (int i = 0; i < SIZE; i++)
         nlength[i] = 0;
@@ -33,19 +34,33 @@ int main()
         }
     }
     // Horizontal orientation
+    printf("%120s", "Horizontal orientation of histogram");
+    printf("\n");
     for (int i = 0; i < SIZE; i++) {
         if (i == SIZE - 1) {
-            printf("> %4d", STEP * (i + 1));
+            printf("> %4d", STEP * i);
         } else {
             printf("%d - %2d", STEP * i, STEP * (i + 1));
         }
         printf(" ");
-        while (nlength[i]-- > 0) {
+        ncat = nlength[i];
+        while (ncat-- > 0) {
             printf("x");
         }
         printf("\n");
     }
     printf("\n");
     // Vertical orientation
+    printf("%120s", "Vertical orientation of histogram");
+    printf("\n");
+    for (int i = 0; i < SIZE; i++)
+        if (threshold < nlength[i]) threshold = nlength[i];
+    while (threshold-- > 0) {
+        for (int i = 0; i < SIZE; i++)
+        {
+            /* code */
+        }
+        
+    }
     
 }
