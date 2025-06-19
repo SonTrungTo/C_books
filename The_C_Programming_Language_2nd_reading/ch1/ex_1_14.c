7d2b2a21192666967d9d57712ce9eb866bc77396
SonTrungTo <son.trung.to@gmail.com>
2025-06-19 07:06:01 +0700

    fix: start to solve 1_14

--- The_C_Programming_Language_2nd_reading/ch1/ex_1_14.c
+++ The_C_Programming_Language_2nd_reading/ch1/ex_1_14.c
@@ -5,5 +5,35 @@
 /* Print a histogram of the frequencies of different characters */
 int main()
 {
-    
+    int c, nb, nothers;
+    int nchars[NCHAR_SIZE];
+
+    nb = 0;
+    nothers = 0;
+    for (int i = 0; i < NCHAR_SIZE; i++)
+       nchars[i] = 0;
+
+    // Need to factor in uppercase vs lowercase letters
+    printf("a: %d\n", 'a');
+    printf("b: %d\n", 'b');
+    printf("z: %d\n", 'z');
+    printf("A: %d\n", 'A');
+    printf("B: %d\n", 'B');
+    printf("Z: %d\n", 'Z');
+
+    // while ((c = getchar()) != EOF) {
+    //     if (c >= 'a')
+    //     {
+    //         /* code */
+    //     }
+    //     else if (condition)
+    //     {
+    //         /* code */
+    //     }
+    //     else
+    //     {
+    //         /* code */
+    //     }
+        
+    // }
 }
