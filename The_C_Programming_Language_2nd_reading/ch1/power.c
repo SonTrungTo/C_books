a417dc8db8b20e5c5d3091bce35915f9f18ca4a6
SonTrungTo <son.trung.to@gmail.com>
2025-06-27 09:47:37 +0300

    fix: explore functions in C

---
+++ The_C_Programming_Language_2nd_reading/ch1/power.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int power(int m, int n);
+
+/* test power function */
+int main()
+{
+    
+}
+
+/* power: raise base to n-th power; n >= 0 */
+int power(int base, int n)
+{
+    int i, p;
+    
+}
