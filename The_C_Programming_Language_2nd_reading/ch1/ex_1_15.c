88a0ce006a635b4a68a6087b1cdc4b32e54fd0e6
SonTrungTo <son.trung.to@gmail.com>
2025-06-29 17:40:49 +0300

    fix: start 1_15.c

---
+++ The_C_Programming_Language_2nd_reading/ch1/ex_1_15.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int temp_conversion(int input_val, char is_celsius);
+
+/* Temperature conversion, function version */
+int main()
+{
+    /* code */
+    return 0;
+}
+
+
+int temp_conversion(int input_val, char is_celsius) {
+    /* code */
+    return 0;
+}
