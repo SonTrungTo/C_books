c655e78011f3eb13d81b463bccee0d7b85fba249
SonTrungTo <son.trung.to@gmail.com>
2025-06-30 02:02:44 +0300

    fix: finish 1_15.c

--- The_C_Programming_Language_2nd_reading/ch1/ex_1_15.c
+++ The_C_Programming_Language_2nd_reading/ch1/ex_1_15.c
@@ -1,16 +1,45 @@
 #include <stdio.h>
 
-int temp_conversion(int input_val, char is_celsius);
+#define     LOWER       0 /* lower limit of table */
+#define     UPPER     300 /* upper limit */
+#define     STEP       20 /* step size */
+
+float temp_conversion(int input_val, char is_celsius);
 
 /* Temperature conversion, function version */
 int main()
 {
-    /* code */
+    char c_flag;
+    int i;
+
+    printf("Fahrenheit----->Celsius table.\n");
+    for (i = LOWER; i <= UPPER; i += STEP)
+        printf("%3d %6.1f\n", i, temp_conversion(i,'c'));
+    
+    printf("Celsius----->Fahrenheit table.\n");
+    for (i = LOWER; i <= UPPER; i += STEP)
+        printf("%3d %6.1f\n", i, temp_conversion(i,'f'));
+
+    printf("Unknown conversion table.\n");
+    for (i = LOWER; i <= UPPER; i += STEP)
+        printf("%3d %6.1f\n", i, temp_conversion(i,'a'));
+
     return 0;
 }
 
 
-int temp_conversion(int input_val, char is_celsius) {
-    /* code */
-    return 0;
+float temp_conversion(int input_val, char is_celsius) {
+    float output_val;
+
+    if (is_celsius == 'c')
+        output_val = 5 * (input_val - 32.0) / 9;
+    else if (is_celsius == 'f')
+        output_val = (9.0 / 5.0) * input_val + 32.0;
+    else
+    {
+        printf("ERROR: Unknown conversion flag!\n");
+        return 1;
+    }
+
+    return output_val;
 }
