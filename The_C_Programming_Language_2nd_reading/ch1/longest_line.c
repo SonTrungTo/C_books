cd260358a9ae3ac1e9cdd8f11ea6ebd79a4cba03
SonTrungTo <son.trung.to@gmail.com>
2025-07-02 00:59:05 +0300

    fix: finish longest_line.c

--- The_C_Programming_Language_2nd_reading/ch1/longest_line.c
+++ The_C_Programming_Language_2nd_reading/ch1/longest_line.c
@@ -1,12 +1,46 @@
 #include <stdio.h>
 #define MAXLINE 1000    /* maximum input line size */
 
-int getline(char line[], int maxline);
+int getlinec(char line[], int maxline);
 void copy(char to[], char from[]);
 
 /* print the longest input line */
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int len;    /* current line length */
+    int max;    /* maximum length seen so far */
+    char line[MAXLINE]; /* current input line */
+    char longest[MAXLINE]; /* longest line saved here */
+    
+    max = 0;
+    while ((len = getlinec(line, MAXLINE)) > 0)
+        if (len > max) {
+            max = len;
+            copy(longest, line);
+        }
+    if (max > 0) /* there was a line */
+        printf("%s", longest);
+
     return 0;
+}
+
+/* getlinec: return the length of each line. An EOF returns 0 */
+int getlinec(char s[], int lim) {
+    int c, i;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++)
+        s[i] = c;
+    /* Newline character to be added next-to-last */
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+
+    return i;
+}
+
+/* copy: copy an array of characters */
+void copy(char to[], char from[]) {
+    for (int i = 0; (to[i] = from[i]) != '\0'; i++);
 }
